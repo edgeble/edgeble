22c279879c5298385dd2c1b5b64a82152caf559b
paritoshwankhade <paritosh21w@gmail.com>
2024-06-09 16:03:24 +0530

    Updated the images and info

--- app/page.tsx
+++ app/page.tsx
@@ -91,11 +91,10 @@ export default function Home() {
                             </div>
                         </div>
                         <div className="flex flex-col">
-                            <img src="./cardimage1.webp" className="w-full w-full object-cover cutImage" />
-                            <p className="text-lg md:text-xl lg:text-2xl font-semibold pt-8 pb-4">AI Accelerator</p>
+                            <img src="./cardimage2.webp" className="w-full w-full object-cover cutImage" />
+                            <p className="text-lg md:text-xl lg:text-2xl font-semibold pt-8 pb-4">Machine Vision Sensors</p>
                             <p>
-                                Performance-driven standardized neural compute modules with built-in AI Accelerators that are ready to deploy in any
-                                AIoT sectors.
+                            Application-agnostic Camera sensor modules built with robust connectivity for delivering ultra vision Edge AI solutions.
                             </p>
                             <div className="flex">
                                 <button className="min-w-content pr-6 pl-4 py-2 mt-2 text-white bg-black hover:border-blue-700 hover:scale-110 hover:translate-x-2 hover:translate-y-2 hover:bg-gradient-to-r hover:from-black hover:to-blue-900 cursor-pointer button2 transition duration-300 transform">
@@ -104,11 +103,10 @@ export default function Home() {
                             </div>
                         </div>
                         <div className="flex flex-col">
-                            <img src="./cardimage1.webp" className="w-full w-full object-cover cutImage" />
-                            <p className="text-lg md:text-xl lg:text-2xl font-semibold pt-8 pb-4">AI Accelerator</p>
+                            <img src="./cardimage3.webp" className="w-full w-full object-cover cutImage" />
+                            <p className="text-lg md:text-xl lg:text-2xl font-semibold pt-8 pb-4">OpenAIA Ecosystem</p>
                             <p>
-                                Performance-driven standardized neural compute modules with built-in AI Accelerators that are ready to deploy in any
-                                AIoT sectors.
+                            Open Source AI Accelerator model management platform which can train, deploy, and update the models powered by DevOps.
                             </p>
                             <div className="flex">
                                 <button className="min-w-content pr-6 pl-4 py-2 mt-2 text-white bg-black hover:border-blue-700 hover:scale-110 hover:translate-x-2 hover:translate-y-2 hover:bg-gradient-to-r hover:from-black hover:to-blue-900 cursor-pointer button2 transition duration-300 transform">
@@ -141,7 +139,7 @@ export default function Home() {
                             }`}
                             onClick={() => setindex(1)}
                         >
-                            Discriminative
+                            Generative
                         </button>
                         <button
                             className={`text-sm lg:text-lg px-4 lg:px-8 py-1 lg:py-2 transition duration-500 transform parallelogram ${
@@ -149,7 +147,7 @@ export default function Home() {
                             }`}
                             onClick={() => setindex(2)}
                         >
-                            Discriminative
+                            Enhansive
                         </button>
                     </div>
                     <div className="mt-8 w-full flex justify-center">
