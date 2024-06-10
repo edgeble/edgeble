f2b03af25308e2a474514fe9e1fced2ae4336c2f
paritoshwankhade <paritosh21w@gmail.com>
2024-06-10 17:55:47 +0530

    removed description

--- app/page.tsx
+++ app/page.tsx
@@ -34,10 +34,6 @@ export default function Home() {
                         </div>
                         <div className="text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Pre-trained GenAI Accelerator for Drone</h1>
-                            <p className="mt-6 text-lg leading-8 text-gray-600">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                                occaecat fugiat aliqua.
-                            </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <a
                                     href="#"
