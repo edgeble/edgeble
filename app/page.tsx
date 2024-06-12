19aff2d8db7e7ffebec791fdebd7d58e38d87983
paritoshwankhade <paritosh21w@gmail.com>
2024-06-13 03:59:29 +0530

    fixed build typos

--- app/page.tsx
+++ app/page.tsx
@@ -304,7 +304,7 @@ export default function Home() {
                         <img src="./1.png" className="w-full object-cover rounded-lg" />
                     </div>
                     <p className="text-sm md:text-lg xl:text-xl 2xl:text-2xl text-center px-4">
-			OpenAIA, EdgebIe AI's advanced AI Accelerator Platform, seamlessly empowers Edge AI-enabled devices to develop, train, deploy, and update AI models, integrating secure and scalable model management with DevOps methodologies to drive transformative innovation.
+			OpenAIA, EdgebIe AI&apos;s advanced AI Accelerator Platform, seamlessly empowers Edge AI-enabled devices to develop, train, deploy, and update AI models, integrating secure and scalable model management with DevOps methodologies to drive transformative innovation.
                     </p>
                     <div className="rotate-90 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
                         <div
