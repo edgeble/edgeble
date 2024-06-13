69ff700c0eeeb9e6896fd81522948dcbb052fe39
paritoshwankhade <paritosh21w@gmail.com>
2024-06-14 03:42:32 +0530

    Fixed the video section and added 2 new videos

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,8 +12,8 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ index, activeIndex, setActiveIndex, title, description }) => {
   const router = useRouter();
   return (
-    <div className="w-[90vw] md:w-1/3 p-1 md:p-2 lg:p-4 cursor-pointer" onClick={() => setActiveIndex(index)}>
-      <div className={`z-0 bg-white flex flex-col border border-2 p-4 lg:p-6 transition-transform duration-300 ease-in-out ${activeIndex === index ? 'border-black' : 'border-gray-200 hover:border-gray-400'}`}>
+    <div className="flex flex-col items-center p-2 cursor-pointer" onClick={() => setActiveIndex(index)}>
+      <div className={`bg-white flex flex-col border border-2 p-4 md:p-6 transition-transform duration-300 ease-in-out ${activeIndex === index ? 'border-black' : 'border-gray-200 hover:border-gray-400'}`}>
         <p className="text-md md:text-lg xl:text-xl 2xl:text-3xl font-semibold mb-1">{title}</p>
         <p className="text-[12px] md:text-sm xl:text-md 2xl:text-lg text-gray-600">{description}</p>
         <div className="w-24 lg:w-36 text-[10px] md:text-[12px] lg:text-[14px] xl:text-sm 2xl:text-md leading-6 text-white cursor-pointer bg-gradient-to-r from-black/50 via-black to-black parallelogram2 px-2 lg:px-4 2xl:px-6 py-px lg:py-1 mt-2 text-center" onClick={() => router.push(`/product/${index}`)}>
--- app/components/MainComponent.tsx
+++ app/components/MainComponent.tsx
@@ -6,26 +6,27 @@ import Card from './Card';
 const MainComponent = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const videoSources = ['./adas1.mp4', './adas2.mp4', './adas3.mp4'];
+  const videoSources = ['./adas1.mp4', './adas2.mp4', './adas3.mp4', './adas4.mp4', './adas5.mp4'];
   const cardData = [
     { title: 'Discriminative', description: 'lorem', index: 0 },
-    { title: 'Discriminative', description: 'lorem', index: 1 },
-    { title: 'Discriminative', description: 'lorem', index: 2 },
-    { title: 'Discriminative', description: 'lorem', index: 3 },
-    { title: 'Discriminative', description: 'lorem', index: 4 },
+    { title: 'Generative', description: 'lorem', index: 1 },
+    { title: 'Enhansive', description: 'lorem', index: 2 },
+    { title: 'Object Detection', description: 'lorem', index: 3 },
+    { title: 'Edgeble Solution', description: 'lorem', index: 4 },
   ];
 
+
   return (
-    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }} className="h-screen w-screen overflow-hidden">
-      <div className="w-full h-full flex flex-col justify-center items-center p-8 md:p-12 lg:p-16 2xl:p-48 relative isolate">
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }} className="min-h-screen w-full overflow-hidden">
+      <div className="w-full h-full flex flex-col justify-center items-center p-4 md:p-8 lg:p-12 2xl:p-16 relative isolate">
         <p className="text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 lg:mb-12 xl:mb-16 2xl:mb-16 text-center">Burgeoning Edge AI</p>
-        <div className="w-full h-full flex flex-col lg:flex-row items-center justify-center overflow-hidden">
+        <div className="w-full flex flex-col lg:flex-row items-center justify-center">
           <div className="order-2 lg:order-1 w-full flex justify-center">
             {videoSources.map((src, index) => (
               <VideoPlayer key={index} src={src} activeIndex={activeIndex} index={index} />
             ))}
           </div>
-          <div className="order-1 lg:order-2 w-full flex flex-row lg:flex-wrap items-center lg:justify-center mb-6 md:mb-0 overflow-x-auto scrollbar-hide">
+          <div className="order-1 lg:order-2 w-full flex flex-wrap items-center justify-center gap-4">
             {cardData.map((card) => (
               <Card key={card.index} index={card.index} activeIndex={activeIndex} setActiveIndex={setActiveIndex} title={card.title} description={card.description} />
             ))}
