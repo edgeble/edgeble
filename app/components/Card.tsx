408f1327790b027181ff52f90cdd918655df2318
paritoshwankhade <paritosh21w@gmail.com>
2024-07-02 05:02:10 +0530

    mobile view

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -16,7 +16,7 @@ const Card: React.FC<CardProps> = ({ index, activeIndex, setActiveIndex, title, 
       <div className={`w-full h-full bg-white flex flex-col border border-2 p-4 md:p-6 transition-transform duration-300 ease-in-out ${activeIndex === index ? 'border-black' : 'border-gray-200 hover:border-gray-400'}`}>
         <p className="text-md md:text-lg xl:text-xl 2xl:text-3xl font-semibold mb-1">{title}</p>
         <p className="text-[12px] md:text-sm xl:text-md 2xl:text-lg text-gray-600">{description}</p>
-        <div className="w-24 lg:w-36 text-[10px] md:text-[12px] lg:text-[14px] xl:text-sm 2xl:text-md leading-6 text-white cursor-pointer bg-gradient-to-r from-black/50 via-black to-black parallelogram2 px-2 lg:px-4 2xl:px-6 py-px lg:py-1 mt-2 text-center" onClick={() => router.push(`/product/${index}`)}>
+        <div className="hidden md:block w-24 lg:w-36 text-[10px] md:text-[12px] lg:text-[14px] xl:text-sm 2xl:text-md leading-6 text-white cursor-pointer bg-gradient-to-r from-black/50 via-black to-black parallelogram2 px-2 lg:px-4 2xl:px-6 py-px lg:py-1 mt-2 text-center" onClick={() => router.push(`/product/${index}`)}>
           Learn more <span aria-hidden="true">→</span>
         </div>
       </div>
