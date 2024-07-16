303008053264f72574fad6ea97074bc388668ba3
paritoshwankhade <paritosh21w@gmail.com>
2024-07-17 02:26:27 +0530

    Adjusted for various screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       screens: {
         '3xl': '1920px', // Custom breakpoint for larger screens
         '4xl': '2560px', // Custom breakpoint for even larger screens
+        '5xl': '3200px'
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
