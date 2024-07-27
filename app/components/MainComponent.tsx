27d3f56f2e2c58e4df7e2d2140912f83997b2b4b
srikanthsiddatam <siddatamsrikanth7@gmail.com>
2024-07-27 17:36:02 +0530

    add the changes in App

--- app/components/MainComponent.tsx
+++ app/components/MainComponent.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import VideoPlayer from './VideoPlayer';
@@ -41,7 +42,7 @@ const MainComponent = () => {
       <VideoPlayer key={index} src={src} activeIndex={activeIndex} index={index} />
     ))}
   </div>
-  <div className="order-1 lg:order-2 w-full lg:w-1/4 grid grid-cols-2 gap-4">
+  <div className="order-1 lg:order-2 w-full lg:w-1/4 grid grid-cols-2 gap-4 mb-[135px]">
     {cardData.map((card) => (
       <Card key={card.index} index={card.index} activeIndex={activeIndex} setActiveIndex={setActiveIndex} title={card.title} imageUrl={card.imageUrl} />
     ))}
@@ -49,7 +50,7 @@ const MainComponent = () => {
 </div>
 
         <div className="rotate-30 absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-          <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-blue-500 to-purple-300 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }} />
+          <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }} />
         </div>
       </div>
     </motion.div>
